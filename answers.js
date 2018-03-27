674fcd83954f8a22878b750e7d9dc539f9379a82
katcozadd <krosecozadd@gmail.com>
2018-03-26 18:08:36 -0600

    2nd commit - created some data structures and accessed some date

--- answers.js
+++ answers.js
@@ -1,15 +1,51 @@
+//Datatypes - Exercise 1
 // Answer the Following
 // For each of the following, write which datatypes you would use to represent the data, and then give a small example of the data structure:
 
 // 1.)A light switch that can be either on or off.
-// Datatype:
+// Datatype:Boolean
+
 // 2.)A user's email address.
-// Datatype: 
+// Datatype:Strong
+
 // 3.)A spaceship with a hull, laser blasters, tractor beam, and warp drive.
-// Datatype: Array
+// Datatype: Object
+
 // 4.)A list of student names from our class.
 // Datatype: Array
+
 // 5.)A list of student names from our class, each with a location.
 // Datatype: Object
+
 // 6.)A list of student names from our class, each with a location and each with a list of favorite tv shows.
 // Datatype: Object
+
+//Take it Easy - Exercise 2
+// 1.)Make an array that holds all of the colors of the rainbow.
+
+// const colorsOfRainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+// 2.)Write code that will access "blue" from the rainbow array.
+
+// colorsOfRainbow[4];
+// console.log(colorsOfRainbow[4]);
+
+// 3.)Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+
+const myObject = {
+	name: 'Kat',
+	favoriteFood: 'mushrooms',
+	hobby: 'drinking wine?',
+	currentCity: 'Denver',
+	favoriteDateType: 'Strings'
+};
+
+// // 4.)Write code that will access your hobby from the object that you just created.
+
+myObject.hobby 
+console.log(myObject.hobby);
+
+
+
+
+
