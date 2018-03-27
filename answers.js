fe2bfda59e8e90b4e48a2d5343857e804dd23d72
katcozadd <krosecozadd@gmail.com>
2018-03-26 18:09:20 -0600

    3rd commit - performed operations on the crazy object

--- answers.js
+++ answers.js
@@ -32,20 +32,68 @@
 
 // 3.)Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
 
-const myObject = {
-	name: 'Kat',
-	favoriteFood: 'mushrooms',
-	hobby: 'drinking wine?',
-	currentCity: 'Denver',
-	favoriteDateType: 'Strings'
-};
+// const myObject = {
+// 	name: 'Kat',
+// 	favoriteFood: 'mushrooms',
+// 	hobby: 'drinking wine?',
+// 	currentCity: 'Denver',
+// 	favoriteDateType: 'Strings'
+// };
 
 // // 4.)Write code that will access your hobby from the object that you just created.
 
-myObject.hobby 
-console.log(myObject.hobby);
+// myObject.hobby 
+// console.log(myObject.hobby);
 
 
+//Crazy Object! - Exercise 3
+
+const crazyObject = {
+  taco: [{meat: 'steak',
+         cheese: ['panela', 'queso', 'chihuahua']},
+         {meat: 'chicken',
+          salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]},
+        ],
+  larry: {
+    	nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    	quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    	characters: [{
+      	name: "Jeff",
+      	occupation: "manager"
+    },
+    {
+     	name: "funkhauser",
+      	occupation: "tv dude"
+    },
+    {
+      	name: "susie",
+      	occupation: "jeffs wife",
+     	favourtieHobby: "Swearing at Larry and Jeff"
+    },
+    ]
+  }
+}
+
+//Use crazy Object to log the following.
+//1.)"omg my mouth is burning"
+
+console.log(crazyObject.taco[1].salsa[5]);
+
+//2.)"Pretty pretty prettayyyyy good"
+
+console.log(crazyObject.larry.quotes[0]);
+
+//3.)"Swearing at Larry and Jeff"
+
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+
+//4.)"Chicken Teriyaki Boyyyyyy"
+
+console.log(crazyObject.larry.nicknames[1]);
+
+//5.)The object the contains the name funkhauser
+
+console.log(crazyObject.larry.characters[1]);
 
 
 
